099e8f709864894a00df024a610ffa9100925b8a
aaronrosenzweig24 <74018820+aaronrosenzweig24@users.noreply.github.com>
2020-11-30 14:48:20 -0500

    Update housingAndCovidProject.R

--- housingAndCovidProject.R
+++ housingAndCovidProject.R
@@ -2,23 +2,19 @@ data = read.csv("housingAndCovidData.csv")
 data$period_begin <- as.Date(data$period_begin, format = "%Y-%m-%d")
 data$period_end <- as.Date(data$period_end, format = "%Y-%m-%d")
 
-# histogram over time
-# Cross correlation of covid and housing irrespective of time
-# Visual assessment and cross correlation (xi - mean) and (yi - mean) if its high there is a strong corrilation
-# regression model covid vs housing
+# This is to create a histogram from manipulated redfin housing data only focusing on the pittsburgh area
+# This is compared to the data from manipulated new york times covid data also focusing on the pittrburgh area
+# The data begins on March 9th of 2020 and ends on November 8th of 2020
 
 library(ggplot2)
 ggplot(mapping = aes(x, y)) +
   geom_bar(data = data.frame(x = data$period_begin, y = data$median_price_mapped), stat = 'identity', fill = "black") +
   geom_bar(data = data.frame(x = data$period_begin, y = data$cases_mapped), stat = 'identity', fill = 'yellow', width = 3) +
   theme_classic() + scale_y_continuous(expand = c(0, 0))
-# This plot uses a scaled representation of the housing and covid cases
-# Mapping to smallest value to 0 and the biggest to 1 so they can be desplayed
-# on the same graph
-# From this we can see that the high number of covid cases has raised the median sale price
-# At the start of covid the median sale price grew and then dropped as cases increased
-# After some time the housing price stayed the same, higher then it was before covid
+
+# We decided to use ggplot because it has the best tools for comparing two peices inforamtion of one graph
+# The built in aes funciton or aesthetic mapping tool helped us to put the data against each other
 
 
 ccf(data$cases, data$median_sale_price)
-# From this cross correlation we can see that covid cases and median sale price are autocorrialted to each other
+# we used the ccf function to make an autocorrelation graph that states that there is a correlation between covid cases and median sale prices 
